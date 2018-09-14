0ade1d40e555ed6259941084197281d543ab6211
Herve Ly-Sunnaram <herve.ly-sunnaram@c-s.fr>
2018-09-14 12:20:08 +0200

    fix package_info
    * allow access to FFTW3*.cmake files for `find_package(FFTW3 CONFIG)` when using cmake_paths generator

--- conanfile.py
+++ conanfile.py
@@ -5,29 +5,20 @@ from conans import ConanFile, CMake, tools
 import os
 
 
-class LibnameConan(ConanFile):
+class FFTWConan(ConanFile):
     name = "fftw"
     version = "3.3.8"
     description = "C subroutine library for computing the discrete Fourier transform (DFT) in one or more dimensions"
     url = "https://github.com/hlysunnaram/conan-fftw"
     homepage = "https://github.com/FFTW/fftw3"
     author = "Herve Ly-Sunnaram <herve.ly-sunnaram@c-s.fr>"
-    # Indicates License type of the packaged library
     license = "GPL-2.0"
-
-    # Packages the license for the conanfile.py
     exports = ["COPYRIGHT"]
-
-    # Remove following lines if the target lib does not use cmake.
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
-
-    # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False], "precision": ["double", "single", "longdouble"]}
     default_options = "shared=False", "fPIC=True", "precision=double"
-
-    # Custom attributes for Bincrafters recipe conventions
     source_subfolder = "source_subfolder"
     build_subfolder = "build_subfolder"
 
@@ -39,8 +30,6 @@ class LibnameConan(ConanFile):
         source_url = "http://www.fftw.org"
         tools.get("{0}/fftw-{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
-
-        # Rename to "source_subfolder" is a convention to simplify later steps
         os.rename(extracted_dir, self.source_subfolder)
 
     def configure_cmake(self):
@@ -65,3 +54,5 @@ class LibnameConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        # allow access to FFTW3*.cmake files for find_package(FFTW3 CONFIG) when using cmake_paths generator
+        self.cpp_info.builddirs = ["", "lib/cmake/fftw3"]
